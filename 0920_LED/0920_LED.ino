50d206c7935a62c1bcdfa632405f1c5439748ee4
Evening218 <yvonne870218@gmail.com>
2023-09-20 19:45:57 +0800

    Create 0920_LED.ino

---
+++ 0920_LED/0920_LED.ino
@@ -0,0 +1,186 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(15, OUTPUT);//Do
+  pinMode(2, OUTPUT);//Re
+  pinMode(0, OUTPUT);//Mi
+  pinMode(4, OUTPUT);//Fa
+  pinMode(16, OUTPUT);//So
+  pinMode(17, OUTPUT);//La
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(15, 1);//Do
+  delay(500);//暫停
+  digitalWrite(15, 0);//
+  delay(500);//暫停
+  digitalWrite(15, 1);//Do
+  delay(500);//暫停
+  digitalWrite(15, 0);//
+  delay(500);//暫停
+  digitalWrite(16, 1);//So
+  delay(500);//暫停
+  digitalWrite(16, 0);//
+  delay(500);//暫停
+  digitalWrite(16, 1);//So
+  delay(500);//暫停
+  digitalWrite(16, 0);//
+  delay(500);//暫停
+  digitalWrite(17, 1);//La
+  delay(500);//暫停
+  digitalWrite(17, 0);//
+  delay(500);//暫停
+  digitalWrite(17, 1);//La
+  delay(500);//暫停
+  digitalWrite(17, 0);//
+  delay(500);//暫停
+  digitalWrite(16, 1);//So
+  delay(500);//暫停
+  digitalWrite(16, 0);//
+  delay(1000);//暫停
+
+  digitalWrite(4, 1);//Fa
+  delay(500);//暫停
+  digitalWrite(4, 0);//
+  delay(500);//暫停
+  digitalWrite(4, 1);//Fa
+  delay(500);//暫停
+  digitalWrite(4, 0);//
+  delay(500);//暫停
+  digitalWrite(0, 1);//Mi
+  delay(500);//暫停
+  digitalWrite(0, 0);//
+  delay(500);//暫停
+  digitalWrite(0, 1);//Mi
+  delay(500);//暫停
+  digitalWrite(0, 0);//
+  delay(500);//暫停
+  digitalWrite(2, 1);//Re
+  delay(500);//暫停
+  digitalWrite(2, 0);//
+  delay(500);//暫停
+  digitalWrite(2, 1);//Re
+  delay(500);//暫停
+  digitalWrite(2, 0);//
+  delay(500);//暫停
+  digitalWrite(15, 1);//Do
+  delay(500);//暫停
+  digitalWrite(15, 0);//
+  delay(1000);//暫停
+
+  digitalWrite(16, 1);//So
+  delay(500);//暫停
+  digitalWrite(16, 0);//
+  delay(500);//暫停
+  digitalWrite(16, 1);//So
+  delay(500);//暫停
+  digitalWrite(16, 0);//
+  delay(500);//暫停
+  digitalWrite(4, 1);//Fa
+  delay(500);//暫停
+  digitalWrite(4, 0);//
+  delay(500);//暫停
+  digitalWrite(4, 1);//Fa
+  delay(500);//暫停
+  digitalWrite(4, 0);//
+  delay(500);//暫停
+  digitalWrite(0, 1);//Mi
+  delay(500);//暫停
+  digitalWrite(0, 0);//
+  delay(500);//暫停
+  digitalWrite(0, 1);//Mi
+  delay(500);//暫停
+  digitalWrite(0, 0);//
+  delay(500);//暫停
+  digitalWrite(2, 1);//Re
+  delay(500);//暫停
+  digitalWrite(2, 0);//
+  delay(1000);//暫停
+
+  digitalWrite(16, 1);//So
+  delay(500);//暫停
+  digitalWrite(16, 0);//
+  delay(500);//暫停
+  digitalWrite(16, 1);//So
+  delay(500);//暫停
+  digitalWrite(16, 0);//
+  delay(500);//暫停
+  digitalWrite(4, 1);//Fa
+  delay(500);//暫停
+  digitalWrite(4, 0);//
+  delay(500);//暫停
+  digitalWrite(4, 1);//Fa
+  delay(500);//暫停
+  digitalWrite(4, 0);//
+  delay(500);//暫停
+  digitalWrite(0, 1);//Mi
+  delay(500);//暫停
+  digitalWrite(0, 0);//
+  delay(500);//暫停
+  digitalWrite(0, 1);//Mi
+  delay(500);//暫停
+  digitalWrite(0, 0);//
+  delay(500);//暫停
+  digitalWrite(2, 1);//Re
+  delay(500);//暫停
+  digitalWrite(2, 0);//
+  delay(1000);//暫停
+
+  digitalWrite(15, 1);//Do
+  delay(500);//暫停
+  digitalWrite(15, 0);//
+  delay(500);//暫停
+  digitalWrite(15, 1);//Do
+  delay(500);//暫停
+  digitalWrite(15, 0);//
+  delay(500);//暫停
+  digitalWrite(16, 1);//So
+  delay(500);//暫停
+  digitalWrite(16, 0);//
+  delay(500);//暫停
+  digitalWrite(16, 1);//So
+  delay(500);//暫停
+  digitalWrite(16, 0);//
+  delay(500);//暫停
+  digitalWrite(17, 1);//La
+  delay(500);//暫停
+  digitalWrite(17, 0);//
+  delay(500);//暫停
+  digitalWrite(17, 1);//La
+  delay(500);//暫停
+  digitalWrite(17, 0);//
+  delay(500);//暫停
+  digitalWrite(16, 1);//So
+  delay(500);//暫停
+  digitalWrite(16, 0);//
+  delay(1000);//暫停
+
+  digitalWrite(4, 1);//Fa
+  delay(500);//暫停
+  digitalWrite(4, 0);//
+  delay(500);//暫停
+  digitalWrite(4, 1);//Fa
+  delay(500);//暫停
+  digitalWrite(4, 0);//
+  delay(500);//暫停
+  digitalWrite(0, 1);//Mi
+  delay(500);//暫停
+  digitalWrite(0, 0);//
+  delay(500);//暫停
+  digitalWrite(0, 1);//Mi
+  delay(500);//暫停
+  digitalWrite(0, 0);//
+  delay(500);//暫停
+  digitalWrite(2, 1);//Re
+  delay(500);//暫停
+  digitalWrite(2, 0);//
+  delay(500);//暫停
+  digitalWrite(2, 1);//Re
+  delay(500);//暫停
+  digitalWrite(2, 0);//
+  delay(500);//暫停
+  digitalWrite(15, 1);//Do
+  delay(500);//暫停
+  digitalWrite(15, 0);//
+  delay(1000);//暫停
+}
